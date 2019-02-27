ac24b32a268eeddfb80dd41e623962188258fa92
Chris Christensen <christopher@vyncahealth.com>
2019-02-27 15:06:42 -0500

    Changed over to command line args for instance url, region, route53 zone. Removed termination after finishing. Added update for Route53.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import boto3
 import datetime
+import getopt
 import os
 import sys
 import time
@@ -7,10 +8,37 @@ from timeit import default_timer as timer
 
 
 start = timer()
+
+def command_line_args(argv):
+  global old_instance_id
+  global instance_region_name
+  global instance_url
+  global hosted_zone_name
+  try:
+    opts, args = getopt.getopt(argv,"hi:r:u:z:",["instance-id=", "region-name=", "instance-url=", "hosted-zone-name="])
+  except getopt.GetoptError:
+    print('useage: python3 app.py -i <Instance ID> -r <Region Name> -u <Instance Route53 URL (Optional)> -z <Route53 hosted zone name>')
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      print('useage: python3 app.py -i <Instance ID> -r <Region Name> -u <Instance Route53 URL (Optional)> -z <Route53 hosted zone name (Optional)>')
+      sys.exit()
+    elif opt in ("-i", "--instance-id"):
+      old_instance_id = arg
+    elif opt in ("-r", "--region-name"):
+      instance_region_name = arg
+    elif opt in ("-u", "--instance-url"):
+      instance_url = arg
+    elif opt in ("-z", "--hosted_zone_name"):
+      hosted_zone_name = arg
+
+command_line_args(sys.argv[1:])
+
 # Setup boto3 and instance variables
-ec2 = boto3.resource('ec2', region_name=os.environ['AWS_DEFAULT_REGION'])
+ec2 = boto3.resource('ec2', region_name=instance_region_name)
 client = boto3.client('ec2')
-instance = ec2.Instance(''.join(sys.argv[1:]))
+instance = ec2.Instance(old_instance_id)
+route53 = boto3.client('route53')
 
 # Variables
 # Variables for date and time
@@ -41,18 +69,46 @@ keyname = instance.key_name
 
 # Get tags for original instance
 tags = instance.tags
-
 tag_specification = [{'ResourceType': 'instance', 'Tags': tags},]
 
+def get_hosted_zone_id(dnsname):
+  global host_id
+  response = route53.list_hosted_zones_by_name(DNSName=dnsname, MaxItems='1')
+  for zone in response["HostedZones"]:
+    host_id = zone['Id']
+    host_id = host_id.replace("/hostedzone/", "")
+
+def change_dns_record(hostedzoneid, source, target, record_type, ttl):
+  try:
+    response = route53.change_resource_record_sets(
+      HostedZoneId=hostedzoneid,
+      ChangeBatch= {
+        'Comment': 'add %s -> %s' % (source, target),
+        'Changes': [
+          {
+            'Action': 'UPSERT',
+            'ResourceRecordSet': {
+              'Name': source,
+              'Type': record_type,
+              'TTL': ttl,
+              'ResourceRecords': [{'Value': target}]
+            }
+          }
+        ]
+      }
+    )
+  except Exception as e:
+    print(e)
+
 # Shutdown original instance
-print("Stopping instance " + instance.id + "...")
+print("\nStopping instance " + instance.id + "...\n")
 instance.stop()
 instance.wait_until_stopped()
-print("Instance " + instance.id + " has been stopped")
+print("Instance " + instance.id + " has been stopped\n")
 
 # Create image of original instance
 # Output is variable for image
-print("Creating image " + imagename + "...")
+print("Creating image " + imagename + "...\n")
 images = instance.create_image (
   # InstanceId=instanceid,
   Name=imagename
@@ -61,16 +117,10 @@ images = instance.create_image (
 imageid = images.id
 image = ec2.Image(str(images.id))
 image.wait_until_exists(Filters=[{'Name': 'state', 'Values': ['available']}])
-print("Image " + imagename + " has been created.")
-
-# Terminate original instance
-print("Terminating instance " + instance.id + "..." )
-instance.terminate()
-instance.wait_until_terminated()
-print("Instance " + instance.id + " has been terminated.")
+print("Image " + imagename + " has been created.\n")
 
 # Create new instance with image from original instance
-print("Creating new instance based on image " + imagename + "...")
+print("Creating new instance based on image " + imagename + "...\n")
 instances = ec2.create_instances (
   ImageId=imageid,
   MinCount=1,
@@ -79,12 +129,21 @@ instances = ec2.create_instances (
   KeyName=keyname,
   SecurityGroupIds=secgroups,
   SubnetId=subnetid,
-  PrivateIpAddress=ipaddress,
+  # PrivateIpAddress=ipaddress,
   TagSpecifications=tag_specification
 )
 instances[0].wait_until_running()
 for instance in instances:
-  print("Instance " + instance.id + " has been created.")
+  new_ipaddress = instance.private_ip_address
+  print("Instance " + instance.id + " has been created.\n")
+
+if instance_url and hosted_zone_name:
+  print("Updating Route53 DNS...\n")
+  get_hosted_zone_id(hosted_zone_name)
+  change_dns_record(host_id, instance_url, new_ipaddress, 'A', 60)
+  print("Route53 DNS has been updated.\n")
+else:
+  print("Route53 not updated.\n")
 
 elapsed_time = timer() - start
 print("This script took " + str(elapsed_time) + " to complete.")
